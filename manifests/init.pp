0df6f36934001f0abfce6a05961f3c35178ece4a
Matt Hope <mhope@adaptavist.com>
2017-10-31 14:05:28 +0000

    added code to install bundle and make instances depende on it

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,15 @@ class dashing_instances_config(
     $dashing_instances    = {},
     ) {
 
+    # ensure the bundle gem is instaled as its needed by the dashing instance
+    ensure_resource ('package', 'bundle', { ensure => 'latest', provider => 'gem'})
+
+    # create default for create_resource calls
+    $create_default = {
+        'require' => 'Package[bundle]'
+    }
+
     Class['dashing'] -> Dashing::Instance<||>
     include dashing
-    create_resources('dashing::instance',  $dashing_instances)
+    create_resources('dashing::instance',  $dashing_instances, $create_default)
 }
